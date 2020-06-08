f9b90074e562dc96b574c73f59f7fc5961db0b05
Denis Eletsky <ksuandi@gmail.com>
2020-06-08 14:59:27 +0700

    remove debug

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import * as mathjs from "mathjs";
-
-
-
 
 import store from './store'
 import './styles/index.css'
 import App from './App'
-
-globalThis.compile = mathjs.compile
 
 ReactDOM.render(
   <React.StrictMode>
